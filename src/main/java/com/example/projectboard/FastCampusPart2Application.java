482effd8e19dab301677aa587fe9b4fbfb3a756e
LSJ <levipikeman@gmail.com>
2023-08-17 16:26:41 +0900

    #7 - 스프링부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은 :
    
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/example/projectboard/FastCampusPart2Application.java
@@ -0,0 +1,13 @@
+package com.example.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusPart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCampusPart2Application.class, args);
+    }
+
+}
